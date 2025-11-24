8325c66cfcce29627063dd0222779e7d834973dd
Manuel Franco Jiménez Mendoza <manuel.jimenez@ucb.edu.bo>
2025-11-23 20:46:38 -0400

    cambios main

--- app/main.py
+++ app/main.py
@@ -1,36 +1,52 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from fastapi.concurrency import run_in_threadpool
+
+from fastapi_pagination import add_pagination
+from app.core.config import settings
 from app.scripts.create_admin import create_default_admin
 from app.scripts.seeds import init_db 
-from app.core.config import settings
-from app.api.v1 import auth, animals, admin_users, favorite_animals, surveys, trivia, vendp, inventario_admin, transacciones, alimentacion, tareas
-from fastapi.concurrency import run_in_threadpool
-#pagination
-from fastapi_pagination import add_pagination
-#scheduler
-from contextlib import asynccontextmanager
 from app.core.scheduler import scheduler, setup_scheduler
+
+from app.api.v1 import (
+    auth, animals, admin_users, favorite_animals, surveys, 
+    trivia, vendp, inventario_admin, transacciones, 
+    alimentacion, tareas
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Iniciando aplicacion...")
+    print("Iniciando ZooConnect API")
+
+    print("Verificando datos semilla")
+    await run_in_threadpool(init_db)
+
+    print("Verificando usuario administrador")
+    await run_in_threadpool(create_default_admin)
+    
+    print("Iniciando Scheduler")
     setup_scheduler()
+    
+    print("Verificacion exitosa")
+    
     yield
     
-    #apagado
-    print("Apagando aplicacion...")
+
+    print("Apagando Zoocoonect")
     if scheduler.running:
         scheduler.shutdown()
         print("APScheduler detenido")
-# app
+    print("ZooConnect API detenida")
+
+
 app = FastAPI(
     title="ZooConnect API",
     lifespan=lifespan,
     description="API para la gestion de un zoologico",
-    version="4.0.0",
+    version="5.0.0",
 )
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
@@ -39,7 +55,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Routers
+
 app.include_router(auth.router, prefix="/zooconnect/auth", tags=["auth"])
 app.include_router(admin_users.router, prefix="/zooconnect/admin_users", tags=["admin"])
 app.include_router(animals.router, prefix="/zooconnect/animals")
@@ -50,16 +66,6 @@ app.include_router(vendp.router, prefix="/zooconnect/security", tags=["Seguridad
 app.include_router(inventario_admin.router, prefix="/zooconnect/inventario", tags=["Poderoso inventario"])
 app.include_router(transacciones.router, prefix="/zooconnect/transacciones", tags=["Entradas y salidas de inventario"])
 app.include_router(alimentacion.router, prefix="/zooconnect/alimentacion", tags=["Alimentacion"])
-app.include_router(tareas.router, prefix="/zooconnect/tareas", tags=["Tareas"])
-add_pagination(app)
-@app.on_event("startup")
-async def startup_event():
-    print("ZooConnect API iniciada")
-    print("Verificando usuario administrador por defecto")
-    await run_in_threadpool(init_db)
-    await run_in_threadpool(create_default_admin)
-    print("Verificacion completa")
+app.include_router(tareas.router, prefix="/zooconnect/tareas", tags=["Tareas"]) 
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    print("ZooConnect API detenida")
+add_pagination(app)
